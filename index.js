f2f33f2e892454cacfb8f7d66b688e9ba72d789a
zeelib1 <52600405+zeelib1@users.noreply.github.com>
2020-08-04 02:01:13 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,104 @@
+// 
+// var hamburgerIcon = document.getElementById('hamburgerIcon');
+// var burger = document.getElementById('hamburger');
+// var closedHamburger = document.getElementById('closedHamburger');
+//
+// function myFunction() {
+//   var x = document.getElementById("hamburgerIcon");
+//   if (x.style.display === "block") {
+//     x.style.display = "none";
+//   } else {
+//     x.style.display = "block";
+//   }
+// }
+//
+// const mq = window.matchMedia("(max-width: 865px)");
+// if (mq.matches) {
+// burger.addEventListener('click', function(){
+// myFunction();
+// })
+// } else {
+//   hamburgerIcon.style.display = "inline";
+// }
+
+// const mq = window.matchMedia( "(min-width: 865px)" );
+// if (mq.matches) {
+//   burger.id = "none";
+// }
+//
+//   function myFunction(){
+//
+  // const mq = window.matchMedia("(max-width: 865px)");
+  // if (mq.matches) {
+//       burger.style.display = 'block';
+//       burger.addEventListener('click', function(){
+//         hamburgerIcon.style.display = "block";
+//         burger.style.display = 'none';
+//         closedHamburger.style.display = 'block';
+//       })
+//     closedHamburger.addEventListener('click', function(){
+//       closedHamburger.style.display = "none";
+//       hamburgerIcon.style.display = "none";
+//       burger.style.display = "block";
+//
+//
+//     })
+// } else {
+//     hamburgerIcon.style.display = "flex";
+//     burger.style.display = "none";
+//     closedHamburger.style.display = "none";
+// }
+//
+//
+//
+// }
+//
+// myFunction();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//
+//   var navbarList = document.getElementById('hamburger');
+//   navbarList.style.display = 'none';
+//   var hamburgerIcon = document.getElementById('hamburgerIcon');
+//   hamburgerIcon.addEventListener('click', function(){
+//
+//   navbarList.style.display = "flex";
+//   })
+//
+//
+//
+// // hamburgerIcon(x); // Call listener function at run time
+// // x.addListener(hamburgerIcon) // Attach listener function on state changes
+// var x = window.matchMedia("(max-width: 865px)");
+// function myFunction(x) {
+//   if (x.matches) { // If media query matches
+//     hamburgerIcon();
+//   }else {
+//     return;
+//   }
+//
+// }
+//
+// myFunction(x);
+// x.addListener(myFunction);
